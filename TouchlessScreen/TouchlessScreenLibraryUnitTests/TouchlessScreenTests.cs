87596df7391c1252193240cfc4a91f5c4dce6e2d
weegeekps <adam@kernelpanicstudios.com>
2014-03-14 18:44:54 -0500

    amorris: Set up Solution and Projects.

---
+++ TouchlessScreen/TouchlessScreenLibraryUnitTests/TouchlessScreenTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TouchlessScreenLibrary;
+
+namespace TouchlessScreenLibraryUnitTests
+{
+    [TestClass]
+    public class TouchlessScreenTests
+    {
+        [TestMethod]
+        public void InstantiateTouchlessScreenClass()
+        {
+            TouchlessScreen tScreen;
+
+            try
+            {
+                tScreen = new TouchlessScreen();
+            }
+            catch (NotImplementedException)
+            {
+                Assert.Inconclusive("Not yet implemented.");
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("An error occured: " + e);
+            }
+        }
+    }
+}
