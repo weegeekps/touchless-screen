8d0709357b05c8a513e8d692c07525bf0d650781
maguttag <maguttag@mail.iastate.edu>
2014-03-27 00:01:57 -0500

    Added Rudimentary Finger Tracking

---
+++ TouchlessScreen/TouchlessScreenLibrary/ContourCreator.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TouchlessScreenLibrary
+{
+    public class ContourCreator
+    {
+        private bool[,] contourPixels;
+        private int width, height, firstX, firstY, nextX, nextY, currX, currY, dirX, dirY;
+        private int[,] rotations;
+
+
+        /// <summary>
+        /// Creates a ContourCreator backed by the array of pixels which form the rough contour
+        /// </summary>
+        /// <param name="contourPixels"></param>
+        public ContourCreator(bool[,] contourPixels)
+        {
+            this.contourPixels = contourPixels;
+            width = contourPixels.GetLength(0);
+            height = contourPixels.GetLength(1);
+            rotations = new int[2, 8];
+            rotations[0, 0] = 1;
+            rotations[1, 0] = 0;
+
+            rotations[0, 1] = 1;
+            rotations[1, 1] = 1;
+
+            rotations[0, 2] = 0;
+            rotations[1, 2] = 1;
+
+            rotations[0, 3] = -1;
+            rotations[1, 3] = 1;
+
+            rotations[0, 4] = -1;
+            rotations[1, 4] = 0;
+
+            rotations[0, 5] = -1;
+            rotations[1, 5] = -1;
+
+            rotations[0, 6] = 0;
+            rotations[1, 6] = -1;
+
+            rotations[0, 7] = 1;
+            rotations[1, 7] = -1;
+        }
+        private void findCornerPoint()
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (contourPixels[x, y])
+                    {
+                        firstX = x;
+                        firstY = y;
+                        return;
+                    }
+                }
+            }
+        }
+
+        private bool findNext()
+        {
+            int dx = (int)(dirX - currX);
+            int dy = (int)(dirY - currY);
+            const int numDirs = 8;
+            int startDir = GetDirectionIndex(dx, dy);
+            int dirIndex;
+            for (int j = startDir; j < startDir + numDirs; j++)
+            {
+                dirIndex = j % numDirs;
+                int x = currX + rotations[0, dirIndex];
+                int y = currY + rotations[1, dirIndex];
+                if (x < 0 || y < 0 || x >= width || y >= height) return false;
+                if (contourPixels[x, y])
+                {
+                    nextX = x;
+                    nextY = y;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Finds the contour from the backed list of points
+        /// </summary>
+        /// <returns></returns>
+        public List<Tuple<int, int>> findContour()
+        {
+            List<Tuple<int, int>> contour;
+            contour = new List<Tuple<int, int>>();
+            findCornerPoint();
+            currX = dirX = firstX;
+            currY = firstY;
+            dirY = firstY - 1;
+            contour.Add(new Tuple<int, int>(currX, currY));
+            bool found;
+
+            do
+            {
+                found = findNext();
+                if (found)
+                {
+                    dirX = currX;
+                    dirY = currY;
+                    contour.Add(new Tuple<int, int>(nextX, nextY));
+                    currX = nextX;
+                    currY = nextY;
+                }
+            } while (found && (nextX != firstX || nextY != firstY));
+
+            return contour;
+        }
+
+        /// <summary>
+        /// Copied from Candesent NUI
+        /// </summary>
+        /// <param name="dx"></param>
+        /// <param name="dy"></param>
+        /// <returns></returns>
+        private int GetDirectionIndex(int dx, int dy)
+        {
+            if (dy == -1)
+            {
+                if (dx == -1)
+                {
+                    return 6;
+                }
+                if (dx == 0)
+                {
+                    return 7;
+                }
+                if (dx == 1)
+                {
+                    return 0;
+                }
+            }
+            if (dy == 0)
+            {
+                if (dx == -1)
+                {
+                    return 5;
+                }
+                if (dx == 1)
+                {
+                    return 1;
+                }
+            }
+            if (dy == 1)
+            {
+                if (dx == -1)
+                {
+                    return 4;
+                }
+                if (dx == 0)
+                {
+                    return 3;
+                }
+                if (dx == 1)
+                {
+                    return 2;
+                }
+            }
+            return 0;
+        }
+    }
+
+
+}
