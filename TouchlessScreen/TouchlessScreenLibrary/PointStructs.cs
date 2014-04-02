c6b078b2123712c9bba8f22ec35f07512060c944
weegeekps <adam@kernelpanicstudios.com>
2014-04-01 21:28:31 -0500

    amorris: Added some structs I have for Points from a past project.

---
+++ TouchlessScreen/TouchlessScreenLibrary/PointStructs.cs
@@ -0,0 +1,15 @@
+﻿namespace TouchlessScreenLibrary
+{
+    public struct Point2d<T>
+    {
+        public T X;
+        public T Y;
+    }
+
+    public struct Point3d<T>
+    {
+        public T X;
+        public T Y;
+        public T Z;
+    }
+}
