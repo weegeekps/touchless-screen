c5829aec63c3c0b70528b8274ee0a93705b808ad
weegeekps <adam@kernelpanicstudios.com>
2014-04-01 21:38:01 -0500

    amorris: Added some constructors to the Point structs.

--- TouchlessScreen/TouchlessScreenLibrary/PointStructs.cs
+++ TouchlessScreen/TouchlessScreenLibrary/PointStructs.cs
@@ -4,6 +4,18 @@
     {
         public T X;
         public T Y;
+
+        public Point2d(T x, T y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public Point2d(Point3d<T> point3d)
+        {
+            this.X = point3d.X;
+            this.Y = point3d.Y;
+        }
     }
 
     public struct Point3d<T>
@@ -11,5 +23,19 @@
         public T X;
         public T Y;
         public T Z;
+
+        public Point3d(T x, T y, T z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public Point3d(Point2d<T> point2d, T z)
+        {
+            this.X = point2d.X;
+            this.Y = point2d.Y;
+            this.Z = z;
+        }
     }
 }
